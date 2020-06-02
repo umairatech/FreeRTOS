67a5b96d7297b7307102d314af9c5c5dc78774c3
imran <imran@umairatech.com>
2020-06-02 14:43:47 +0530

    First Commit

---
+++ src/main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: 02-Jun-2020
+ *      Author: imranfarheen
+ */
+
+#include "stm32f4xx.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vTaskHandler1(void *);
+void vTaskHandler2(void *);
+
+TaskHandle_t xTaskHandle1 = NULL;
+TaskHandle_t xTaskHandle2 = NULL;
+
+int main(void){
+
+	xTaskCreate(vTaskHandler1,"Task-1",configMINIMAL_STACK_SIZE,NULL,2,&xTaskHandle1);
+	xTaskCreate(vTaskHandler2,"Task-2",configMINIMAL_STACK_SIZE,NULL,2,&xTaskHandle2);
+
+	return 0;
+}
+
+void vTaskHandler1(void *parms)
+{
+	while(1)
+	{
+
+	}
+}
+
+void vTaskHandler2(void *parms)
+{
+	while(1)
+	{
+
+	}
+}
